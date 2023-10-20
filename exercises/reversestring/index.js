99117b947466d183c94c46a0a7a26c53fe749cd8
kanta1207 <kan0712livepool@gmail.com>
2023-10-20 07:55:50 -0700

    first commit

---
+++ exercises/reversestring/index.js
@@ -0,0 +1,17 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+  let reverse = '';
+  for (let i = 0; i < str.length; i++) {
+    reverse = str[i] + reverse;
+  }
+  return reverse;
+}
+
+module.exports = reverse;
