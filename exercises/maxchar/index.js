63beffe41d689901a03043c474f23f92633abf53
kanta1207 <kan0712livepool@gmail.com>
2023-11-02 11:14:19 -0700

    Solve maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const hash = {};
+  let max = 0;
+  let maxChar = str[0];
+  for (let i = 0; i < str.length; i++) {
+    if (hash[str[i]]) {
+      hash[str[i]]++;
+    } else {
+      hash[str[i]] = 1;
+    }
+    if (hash[str[i]] > max) {
+      max = hash[str[i]];
+      maxChar = str[i];
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
